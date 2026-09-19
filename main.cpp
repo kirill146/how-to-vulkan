8f53992ddea47ade7966d95885abf71153d395c6
Kirill Kozlov <drxu59@gmail.com>
2026-09-20 02:01:38 +0500

    Add GLM and fill vertex data

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
+#include <glm/glm.hpp>
+
 void CheckVkResult(VkResult res, const char* file, int line) {
   if (res != VK_SUCCESS) {
     throw std::runtime_error(file + std::string(":") + std::to_string(line) + ": failed with VkResult " + std::to_string(res));
@@ -30,6 +32,12 @@ void CheckSDLResult(bool res, const char* file, int line) {
   }
 }
 #define SDL_CHECK(x) CheckSDLResult(x, __FILE__, __LINE__)
+
+struct Vertex {
+  glm::vec3 pos;
+  glm::vec3 normal;
+  glm::vec2 uv;
+};
 
 void run(uint32_t deviceIndex) {
   VK_CHECK(volkInitialize());
@@ -221,7 +229,38 @@ void run(uint32_t deviceIndex) {
   if (!tinyobj::LoadObj(&attrib, &shapes, &materials, nullptr, nullptr, "assets/suzanne.obj")) {
     throw std::runtime_error("Can't load .obj");
   }
+  const VkDeviceSize indexCount(shapes[0].mesh.indices.size());
+  std::vector<Vertex> vertices;
+  std::vector<uint16_t> indices;
+  // Load vertex and index data
+  for (auto& index : shapes[0].mesh.indices) {
+    Vertex v{
+      .pos = { attrib.vertices[index.vertex_index * 3], -attrib.vertices[index.vertex_index * 3 + 1], attrib.vertices[index.vertex_index * 3 + 2] },
+      .normal = { attrib.normals[index.normal_index * 3], -attrib.normals[index.normal_index * 3 + 1], attrib.normals[index.normal_index * 3 + 2] },
+      .uv = { attrib.texcoords[index.texcoord_index * 2], 1.0 - attrib.texcoords[index.texcoord_index * 2 + 1] }
+    };
+    vertices.push_back(v);
+    indices.push_back(indices.size());
+  }
+  VkDeviceSize vBufSize = sizeof(Vertex) * vertices.size();
+  VkDeviceSize iBufSize = sizeof(uint16_t) * indices.size();
+  VkBufferCreateInfo bufferInfo{
+    .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+    .size = vBufSize + iBufSize,
+    .usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT
+  };
+  VmaAllocationCreateInfo vBufferAllocCI{
+    .flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT,
+    .usage = VMA_MEMORY_USAGE_AUTO
+  };
+  VmaAllocationInfo vBufferAllocInfo;
+  VmaAllocation vBufferAllocation;
+  VkBuffer vBuffer;
+  VK_CHECK(vmaCreateBuffer(allocator, &bufferInfo, &vBufferAllocCI, &vBuffer, &vBufferAllocation, &vBufferAllocInfo));
+  memcpy(vBufferAllocInfo.pMappedData, vertices.data(), vBufSize);
+  memcpy(((char*)vBufferAllocInfo.pMappedData) + vBufSize, indices.data(), iBufSize);
 
+  vmaDestroyBuffer(allocator, vBuffer, vBufferAllocation);
   vkDestroyImageView(device, depthImageView, nullptr);
   vmaDestroyImage(allocator, depthImage, depthImageAllocation);
   vkDestroySwapchainKHR(device, swapchain, nullptr);
